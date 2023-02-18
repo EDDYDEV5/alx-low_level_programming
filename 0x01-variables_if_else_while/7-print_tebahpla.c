10b7d66710af0c137df6a537206c12244de7af57
cletus obosi <cletusobosi5@gmail.com>
2023-02-17 18:58:14 -0800

    7-print_tebahpla.c

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ *main - Entry point
+ *Return: Always 0 (success)
+ */
+int main(void)
+{
+char z;
+
+c = 'z'
+while
+(c <= 'a')
+{putchar(c);
+z--;
+}
+putchar('\n')
+return (0);
+}
