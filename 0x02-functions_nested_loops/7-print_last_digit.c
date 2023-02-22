d2747a2e5c85b1da818eb5faf06ec48041a0c373
cletus obosi <cletusobosi5@gmail.com>
2023-02-22 08:23:49 -0800

    7-print_last_digit.c

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -10,12 +10,15 @@ int print_last_digit(int n)
 {
 int a;
 
-a = n % 10;
+	if (n < 0)
+		n = -n;
 
-if (n > 0)
-{
-return (a);
-}
-else
-return (-a);
+	a = n % 10;
+
+	if (a < 0)
+		a = -a;
+
+	_putchar(a + '0');
+
+	return (a);
 }
