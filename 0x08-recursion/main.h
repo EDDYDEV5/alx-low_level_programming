f59576801ee682908acd6c427f1253bfe2f03184
cletus obosi <cletusobosi5@gmail.com>
2023-03-08 20:46:57 -0800

    0-puts_recursion.c

---
+++ 0x08-recursion/main.h
@@ -0,0 +1,12 @@
+#ifndef main_h
+#define main_h
+
+/**
+ * File: main.h
+ * Desc: Header file containing prototypes for all functions
+ *       used in the 0x08-Recursion directory.
+ */
+
+void _puts_recursion(char *s);
+
+#endif
