8849bc94e338cbe52af6ea71b22f211e5eeab6a6
cletus obosi <cletusobosi5@gmail.com>
2023-03-10 19:10:30 -0800

    libmy.a

---
+++ 0x09-static_libraries/9-strcpy.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * _strcpy - this fuction copy the pointer to variable.
+ * @src: is the pointer is the origin.
+ * @dest: is the pointer is the destiny.
+ * Return: d.
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+return (0);
+}
