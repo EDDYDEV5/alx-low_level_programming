ef7b405104e105ffa0df16a7c91b000105a86fa1
cletus obosi <cletusobosi5@gmail.com>
2023-04-12 21:31:48 -0700

    1-create_file.c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -9,13 +9,13 @@
 
 int create_file(const char *filename, char *text_content)
 {
-int x, i, len = 0;
+int o, w, len = 0;
 
 if (filename == NULL)
 return (-1);
 
-i = open("filename", O_RDWR | O_CREAT | O_TRUNC, 0600);
-if (i == -1)
+o = open("filename", O_RDWR | O_CREAT | O_TRUNC, 0600);
+if (o == -1)
 return (-1);
 
 if (text_content != NULL)
@@ -23,11 +23,11 @@ if (text_content != NULL)
 for (len = 0; text_content[len];)
 len++;
 }
-x = write(i, text_content, len);
-if (x == -1)
+w = write(o, text_content, len);
+if (w == -1)
 return (-1);
 
 
-close(i);
+close(o);
 return (1);
 }
